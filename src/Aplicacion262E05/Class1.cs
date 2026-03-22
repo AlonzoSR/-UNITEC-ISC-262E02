ba8fff2e584c3cabbd0cab1dcfa9254c8c8263e6
Cardarkoo <carlossanchsws1092@gmail.com>
2026-03-22 00:26:49 -0600

    Init

---
+++ src/Aplicacion262E05/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class Rectangulo262E02 {
+public static double Perimetro(double lado_a, double lado_b) {
+return 2*lado_a + 2*lado_b;
+}
+public static double Area(double lado_a, double lado_b) {
+return lado_a * lado_b;
+}
+public static double Volumen(double area, double altura) {
+return area * altura;
+}
+}
---
+++ src/ConsoleApp262E05/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+double lado_a = 5;
+double lado_b = 7;  
+double altura = 10;
+double perimetro = Rectangulo262E02.Perimetro(lado_a, lado_b);
+double area = Rectangulo262E02.Area(lado_a, lado_b);
+double volumen = Rectangulo262E02.Volumen(area, altura);
+
+Console.WriteLine($"El perimetro de un rectangulo de lado {lado_a}cm y de lado {lado_b}cm es {perimetro}cm");
+Console.WriteLine($"El area de un rectangulo de lado {lado_a}cm y de lado {lado_b}cm es {area}cm2");
+Console.WriteLine($"El volumen de un rectangulo con un area de la base {area}cm2 y {altura}cm de altura es {volumen}cm3");
